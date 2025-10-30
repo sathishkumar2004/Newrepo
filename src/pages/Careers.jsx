02db82b6328dba824918e52e1747bea66772430a
Sathishkumar <ssathishkumar0009@gmail.com>
2025-10-30 17:33:37 +0530

    Updated project with latest changes

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import BuildCareer from "../careerpages/buildCareer";
 import WorkWithUs from "../careerpages/WorkWithUs";
 import CareerOpenings from "../careerpages/CareerOpenings";
 import CareerCulture from "../careerpages/CareerCulture";
+import BuildCareer from "../careerpages/buildCareer.jsx";
+
 
 export default function Careers() {
   return (
